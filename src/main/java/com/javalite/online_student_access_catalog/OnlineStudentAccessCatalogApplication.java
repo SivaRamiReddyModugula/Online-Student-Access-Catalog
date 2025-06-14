f8d08d7182588c3e71d0f7c905be5f1ef0c8c01f
Siva Rami Reddy <SivaRamiReddyModugula>
2025-06-14 21:57:51 +0530

    Initial Commit.

---
+++ src/main/java/com/javalite/online_student_access_catalog/OnlineStudentAccessCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.javalite.online_student_access_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStudentAccessCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStudentAccessCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javalite/online_student_access_catalog/OnlineStudentAccessCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalite.online_student_access_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStudentAccessCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
